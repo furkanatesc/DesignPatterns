609dbcc825ae6c1dd1c9bec5b8b86f7990fe7139
furkanatesc <furkanatesc@hotmail.com>
2025-09-08 19:07:00 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+            Console.ReadKey();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Todo business to decide factory
+            return new EdLogger();
+        }
+
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Todo business to decide factory
+            return new FaLogger();
+        }
+
+    }
+
+    public interface ILoggerFactory
+    {
+         ILogger CreateLogger();
+    }
+
+    
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+    }
+
+
+    public class FaLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with FaLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+                
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager=CustomerManager.CreateAsSingleton();
+        }
+    }
+
+    class CustomerManager
+    {
+        static object _lockObject = new object(); //Thread Safe Singleton için
+        private static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            };
+            
+
+            return _customerManager;
+        }
+    }
+}
