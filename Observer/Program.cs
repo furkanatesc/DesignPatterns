fb5ddaf5971368842e2e443d2023a5ba15642e0a
furkanatesc <furkanatesc@hotmail.com>
2025-10-03 21:24:57 +0300

    Required

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -10,6 +10,55 @@ namespace Observer
     {
         static void Main(string[] args)
         {
+            ProductManager productManager = new ProductManager();
+            productManager.Attach(new CustomerObserver());
+            productManager.UpdatePrice();
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> _observers = new List<Observer>();
+        public void UpdatePrice()
+        {
+            Console.WriteLine("pRODUCT PRİCE CHANGED");
+            Notify();
+
+        }
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+        private void Notify()
+        {
+            foreach(var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to customer:Product price changed!");
+        }
+    }
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message To customer : Product price changed");
         }
     }
 }
